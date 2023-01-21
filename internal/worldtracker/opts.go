8089378d574632e93b9bf5eb059df3e82b4418c0
Joey Colon <contact@joey.dev>
2023-01-21 08:55:51 -0800

    tweak default threshold

--- internal/worldtracker/opts.go
+++ internal/worldtracker/opts.go
@@ -5,7 +5,7 @@ import (
 )
 
 type WorldTrackerOpts struct {
-	Threshold int    `short:"t" long:"threshold" description:"The threshold for the number of players to trigger a world tracker spike event" default:"12" required:"true"`
+	Threshold int    `short:"t" long:"threshold" description:"The threshold for the number of players to trigger a world tracker spike event" default:"25" required:"true"`
 	Filter    string `short:"f" long:"filter" description:"The filter for the world tracker spike event" default:"f2p"`
 	Time      int    `short:"i" long:"time" description:"The time in seconds to wait before checking for spikes again" default:"12" required:"true"`
 }
