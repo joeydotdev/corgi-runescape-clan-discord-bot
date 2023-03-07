03b06de3e03e6e1bdd5bca3d3374b2c36d1c8ae7
Joey Colon <contact@joey.dev>
2023-03-06 17:50:07 -0800

    finish masspm plugin

--- internal/plugins/masspm.go
+++ internal/plugins/masspm.go
@@ -1,6 +1,7 @@
 package plugins
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/bwmarrin/discordgo"
@@ -9,6 +10,26 @@ import (
 const (
 	MassPMCommandPluginName = "MassPMCommandPlugin"
 	MassPMChannelID         = "1070035899221545171"
+)
+
+var (
+	ROLE_IDS []string = []string{
+		"692876249118539817",  // Leader
+		"1070076373781205143", // PK Leader
+		// "692879184024043540",  // High Council
+		// "692879285417017375",  // Council
+		// "817499802148274226",  // Leadership
+		// "692879600380018699",  // Officer
+		// "692879942777569312",  // Legend
+		// "1024119526801023006", // Old School
+		// "692880299855446106",  // Veteran
+		// "699354924185682031",  // Advanced
+		// "692880390440091659",  // Member
+		// "773216677423874048",  // Applicant
+	}
+	EXCLUDED_USER_IDS []string = []string{
+		"223169696055296011", // joey
+	}
 )
 
 type MassPMCommandPlugin struct{}
@@ -35,6 +56,63 @@ func (p *MassPMCommandPlugin) Validate(session *discordgo.Session, message *disc
 
 // Execute executes MassPMCommandPlugin on an incoming Discord message.
 func (p *MassPMCommandPlugin) Execute(session *discordgo.Session, message *discordgo.MessageCreate) error {
-	_, err := session.ChannelMessageSend(message.ChannelID, "pong")
+	// parse everything after !masspm
+	segments := strings.Split(message.Content, " ")
+	if len(segments) < 2 {
+		return TooFewArgumentsError
+	}
+
+	// get the message to send
+	messageToSend := strings.Join(segments[1:], " ")
+	if len(messageToSend) == 0 {
+		return TooFewArgumentsError
+	}
+
+	members, err := session.GuildMembers(message.GuildID, "", 1000)
+	if err != nil {
+		return err
+	}
+
+	messageDispatcher := func(member *discordgo.Member) {
+		for _, excludedUserID := range EXCLUDED_USER_IDS {
+			if member.User.ID == excludedUserID {
+				return
+			}
+		}
+
+		hasAttemptedToMessageMember := false
+		for _, role := range member.Roles {
+			if hasAttemptedToMessageMember {
+				break
+			}
+			for _, roleID := range ROLE_IDS {
+				if hasAttemptedToMessageMember {
+					break
+				}
+				if role == roleID {
+					channel, err := session.UserChannelCreate(member.User.ID)
+					if channel == nil || err != nil {
+						fmt.Println("Failed to create channel with member: ", member.User.ID)
+						hasAttemptedToMessageMember = true
+						continue
+					}
+
+					_, err = session.ChannelMessageSend(channel.ID, messageToSend)
+					if err != nil {
+						fmt.Println("Failed to send message to member: ", member.User.ID)
+						fmt.Println("error: ", err)
+					}
+
+					hasAttemptedToMessageMember = true
+				}
+			}
+		}
+	}
+
+	for _, member := range members {
+		go messageDispatcher(member)
+	}
+
+	_, err = session.ChannelMessageSend(message.ChannelID, "Mass PM sent!")
 	return err
 }
